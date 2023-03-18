d6e838a8afe8b9f20727f9e7c5b4c1f3de8d23dd
arham-bsce22007 <bsce22007@itu.edu.pk>
2023-03-18 12:16:03 +0500

    done

---
+++ Vehicle.cpp
@@ -0,0 +1,5 @@
+//
+// Created by HP on 3/18/2023.
+//
+
+#include "Vehicle.h"
---
+++ Vehicle.h
@@ -0,0 +1,31 @@
+//
+// Created by HP on 3/18/2023.
+//
+
+#ifndef GARAGE_VEHICLE_H
+#define GARAGE_VEHICLE_H
+
+#endif //GARAGE_VEHICLE_H
+#include<iostream>
+using namespace std;
+
+class Vehicle{
+protected:
+    int noOfDoors;
+    int noOfTyres;
+    int noOfSeats;
+    int engineCC;
+    string numPlate;
+    string color;
+    string transmissionType;
+public:
+    Vehicle(){
+        noOfDoors = 0;
+        noOfSeats = 0;
+        noOfTyres = 0;
+        engineCC = 0;
+        numPlate = "";
+        color = "";
+        transmissionType = "";
+    }
+};
