fa29b5bf7066835951ef69ac04a91a1ffa68c6bb
arham-bsce22007 <bsce22007@itu.edu.pk>
2023-03-18 15:32:20 +0500

    Update Vehicle.cpp

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -5,10 +5,10 @@
 #include "Vehicle.h"
 
 void Vehicle::generalInput(){
-    cout<<"\nEnter no of Doors:\n";
-    cin>>noOfDoors;
-    cout<<"Enter no of Seats:\n";
-    cin>>noOfSeats;
+    cout<<"\nEnter no of Doors:\n";//bat suno apni apni Car k operator overloading m apni car k mutabik
+    cin>>noOfDoors;                // doors, seats, or tyres k check laga dena refrence k liye SportsCar ki cpp
+    cout<<"Enter no of Seats:\n";  // file m cin ki operator over loading check karo....
+    cin>>noOfSeats;                // :D I believe in you ( ´･･)ﾉ(._.`)
     cout<<"Enter no of Tyres:\n";
     cin>>noOfTyres;
     cout<<"Enter Engine Capacity:\n";
